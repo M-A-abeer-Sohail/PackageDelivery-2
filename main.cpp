ef80464f75edf33af7a8ed67fc828c3ed8867f3a
M-A-abeer-Sohail <60892508+M-A-abeer-Sohail@users.noreply.github.com>
2020-03-11 11:42:08 +0500

    Testing main.cpp to read from file (1)

--- main.cpp
+++ main.cpp
@@ -1,24 +1,43 @@
-#include <iostream>
+#pragma once
 #include <stdio.h>
 #include <stdlib.h>
-#include "truck.hpp"
-#include "node.hpp"
-#include "box.hpp"
-
+#include <iostream>
+#include <fstream>
+#include <string>
+// #include "truck.hpp"
+// #include "node.hpp"
+// #include "box.hpp"
 
 using namespace std;
 
-
-
-
-
+// int main()
+// {
+//     TruckDelivery delivery;
+//     delivery.loadTrucks();
+//     delivery.calculateCost();
+//     delivery.makeJourney();
+//     delivery.unloadTrucks();
+//     return 0;
+// }
 
 int main()
 {
-    TruckDelivery delivery;
-    delivery.loadTrucks();
-    delivery.calculateCost();
-    delivery.makeJourney();
-    delivery.unloadTrucks();
+    char line[100];
+    int count;
+    int n;
+    ifstream myfile("Drivers.txt");
+    if (myfile.is_open())
+    {
+        while (getline(myfile, line))
+        {
+
+            cout << line << '\n';
+        }
+        myfile.close();
+    }
+
+    else
+        cout << "Unable to open file";
+
     return 0;
 }
