e793eeef30dde8d5529381989b06a956de3b9f09
M-A-abeer-Sohail <60892508+M-A-abeer-Sohail@users.noreply.github.com>
2020-03-12 21:24:15 +0500

    Deleted all in box.cpp

--- box.cpp
+++
@@ -1,9 +0,0 @@
-#include "box.hpp"
-#include <stdlib.h>
-
-Box::Box(int a, int b)
-{
-    length = rand() % (31 - 5) + 5;
-    breadth = rand() % (31 - 5) + 5;
-    height = rand() % (31 - 5) + 5;
-}
